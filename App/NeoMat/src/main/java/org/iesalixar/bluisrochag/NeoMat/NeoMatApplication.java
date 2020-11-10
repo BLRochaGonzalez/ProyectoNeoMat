f200ab78a6867e1d1ebd5d79cb015f2a2372aec5
BLRochaGonzalez <alum.bluisrochag@iesalixar.org>
2020-11-10 17:15:04 +0100

    v 0.1.8 CreateSpringProject

---
+++ App/NeoMat/src/main/java/org/iesalixar/bluisrochag/NeoMat/NeoMatApplication.java
@@ -0,0 +1,13 @@
+package org.iesalixar.bluisrochag.NeoMat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoMatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoMatApplication.class, args);
+	}
+
+}
---
+++ App/NeoMat/src/test/java/org/iesalixar/bluisrochag/NeoMat/NeoMatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.bluisrochag.NeoMat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoMatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
