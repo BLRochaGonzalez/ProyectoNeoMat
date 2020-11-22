6dffc0cc8e1c18ecb9285a280570eb2bd2f36b1a
BLRochaGonzalez <alum.bluisrochag@iesalixar.org>
2020-11-22 21:44:24 +0100

    v 0.2.5 Create index.html + myOwnCss.css + myOwJS.js

---
+++ App/NeoMat/src/main/resources/static/js/myOwnJS.js
@@ -0,0 +1,9 @@
+function showRegisterSection(){
+    document.getElementById("loginSection").style.display = "none";
+    document.getElementById("registerSection").style.display = "block";
+}
+
+function showLoginSection(){
+    document.getElementById("registerSection").style.display = "none";
+    document.getElementById("loginSection").style.display = "block";
+}
